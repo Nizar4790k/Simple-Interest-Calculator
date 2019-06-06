03255db24732f4915807199f6b83a6028e3f5fde
Nizar Sanchez <nizar4790k@gmail.com>
2019-06-06 05:02:41 -0400

    Added 2 textfield in the view

--- interest_calculator/lib/main.dart
+++ interest_calculator/lib/main.dart
@@ -1,4 +1,93 @@
-void main (){
+import 'package:flutter/material.dart';
+
+void main ()=> runApp(
+
+  MaterialApp(
 
 
-}
+
+    home: Scaffold(
+
+      appBar: AppBar(
+      title: Text("Crappy Interst Calculator"),
+      ),
+
+      body: Container(
+
+
+        child: Column(
+
+        children: <Widget>[
+
+         TextField(
+           decoration: InputDecoration(
+             labelText: "Enter the principal"
+           ),
+         ),
+
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Enter the rate of interest"
+            ),
+          ),
+
+
+
+          /*
+
+          Row(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Enter the term"
+                ),
+
+              )
+
+
+
+            ],
+          ),
+
+
+          Row(
+            children: <Widget>[
+
+              RaisedButton(
+                child: Text("Calculate"),
+              ),
+
+              RaisedButton(
+                child: Text("Delete!"),
+
+            ),
+
+
+
+            ],
+
+
+          )
+
+*/
+
+
+
+
+
+        ],
+        ),
+
+
+      ),
+
+
+
+
+    )
+
+
+
+  )
+
+);
